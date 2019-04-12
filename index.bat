2ae34e5d4ec936c435c416e1bdaf860f6bbb4ba4
sky-gg <33249562+sky-gg@users.noreply.github.com>
2019-04-12 13:42:24 +0800

    Update index.bat

--- index.bat
+++ index.bat
@@ -1,5 +1,8 @@
 
 cd C:\Users\[用户名]\Desktop
+start C:\Progra~2\Google\Chrome\Application\chrome.exe "https://juejin.im"
+start C:\Progra~2\Tencent\WeChat\WeChat.exe
+code D:\xiangmu\aini
 node a
 
 exit
