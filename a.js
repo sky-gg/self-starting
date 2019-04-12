a5cf07fdfa714a83d3dff8e033f599ffae2d78f3
sky-gg <33249562+sky-gg@users.noreply.github.com>
2019-04-12 13:55:05 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,17 +1,18 @@
 let path = require('path');
 let cp = require('child_process')
 let schedule = require('node-schedule');
+let fs = require('fs');
 const scheduleCronstyle = () => {
   //每天的十一点五十执行一次:
-  schedule.scheduleJob('00 50 11 * * *', () => {/** 秒 分 时 */
-    initApp("该去吃饭了");
+  schedule.scheduleJob('45 54 13 * * *', () => {/** 秒 分 时 */
+    initApp();
   });
 }
 
 scheduleCronstyle();
 function initApp(sweetTip) {
   let vbsPath = path.join(__dirname, 'c.vbs')
-   cp.exec('cscript.exe ' + vbsPath + "温馨提示" + sweetTip , function(err, stdout, stderr) {
+  cp.exec('cscript.exe ' + vbsPath + ' "温馨提示" "该去吃饭了"', function(err, stdout, stderr) {
     if (err) {
       fs.writeFileSync('log.log', err.toString())
     }
