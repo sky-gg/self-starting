095376bc9635641b8102490740c8da6b1c427d74
sky-gg <33249562+sky-gg@users.noreply.github.com>
2019-04-12 14:05:49 +0800

    Update index.bat

--- index.bat
+++ index.bat
@@ -1,8 +1,14 @@
-
-cd C:\Users\[用户名]\Desktop
+REM 打开网站示例
 start C:\Progra~2\Google\Chrome\Application\chrome.exe "https://juejin.im"
+
+REM 打开应用程序示例
 start C:\Progra~2\Tencent\WeChat\WeChat.exe
+
+REM 打开项目示例
 code D:\xiangmu\aini
-node a
+
+REM 执行主文件
+cd C:\Users\[用户名]\Desktop
+node index
 
 exit
