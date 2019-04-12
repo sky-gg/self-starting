8813c5cda909f7c06ab168bc2686d23ec6428dc9
sky-gg <33249562+sky-gg@users.noreply.github.com>
2019-04-12 14:00:56 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
+let schedule = require('node-schedule');
 let path = require('path');
 let cp = require('child_process')
-let schedule = require('node-schedule');
 let fs = require('fs');
 const scheduleCronstyle = () => {
   //每天的十一点五十执行一次:
@@ -8,13 +8,13 @@ const scheduleCronstyle = () => {
     initApp();
   });
 }
-
 scheduleCronstyle();
-function initApp(sweetTip) {
-  let vbsPath = path.join(__dirname, 'c.vbs')
+
+function initApp() {
+  let vbsPath = path.join(__dirname, 'msgbox.vbs')
   cp.exec('cscript.exe ' + vbsPath + ' "温馨提示" "该去吃饭了"', function(err, stdout, stderr) {
     if (err) {
-      fs.writeFileSync('log.log', err.toString())
+      fs.writeFileSync('error.log', err.toString())
     }
   })
 }
